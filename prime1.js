39dd90f5c95afe9360bd46f45d463ec27e85b8bc
Alisha <8282alishakhan@gmail.com>
2023-02-09 17:17:23 +0530

    primeNumber

---
+++ prime1.js
@@ -0,0 +1,10 @@
+ let num=13; 
+let count=0;
+    for(let i=2;i<num;i++)
+    {
+        if(num%i==0)
+            count++;
+    }  
+    if(count==1)
+        console.log("Yes");
+        else console.log("No");
